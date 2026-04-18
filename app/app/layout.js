35e100d768d8a005397c91c0172df2060c91af44
Mandakkk <91105296+Mandakkk@users.noreply.github.com>
2026-04-18 13:10:49 +0800

    Create layout.js

---
+++ app/app/layout.js
@@ -0,0 +1,8 @@
+export const metadata = { title: 'SolarEase' }
+export default function RootLayout({ children }) {
+  return (
+    <html lang="mn">
+      <body>{children}</body>
+    </html>
+  )
+}
